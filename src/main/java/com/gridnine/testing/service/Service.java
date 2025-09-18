299a4172ef53a1e695f0da932b8fb1a4f4fcdef6
1в <cykalov.aleksand@mail.ru>
2025-09-19 00:53:22 +0300

    второй вариант решения задачи, обращение к фильтрам через интерфейс

--- src/main/java/com/gridnine/testing/service/Service.java
+++ src/main/java/com/gridnine/testing/service/Service.java
@@ -2,25 +2,20 @@ package com.gridnine.testing.service;
 
 import com.gridnine.testing.interfaces.Filters;
 import com.gridnine.testing.model.Flight;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Service {
-   private final   List<Filters>filters;
+    private final List<Filters> filters;
 
     public Service(List<Filters> filters) {
-      this.filters = filters;
+        this.filters = filters;
     }
 
-    public List<Filters> getFilters() {
-        return filters;
-    }
-
-    public List<Flight> filter(){
-        List<Flight>result=new ArrayList<>();
-       for (Filters variable:filters){
-            if(!variable.filter()){
+    public List<Flight> filter() {
+        List<Flight> result = new ArrayList<>();
+        for (Filters variable : filters) {
+            if (!variable.filter()) {
                 result.add((Flight) variable);
             }
         }
--- src/test/java/com/gridnine/testing/service/ServiceTest.java
+++ src/test/java/com/gridnine/testing/service/ServiceTest.java
@@ -1,42 +1,54 @@
 package com.gridnine.testing.service;
 
-import com.gridnine.testing.filters.FilterArrivalDateBeforeDeparture;
 import com.gridnine.testing.filters.FilterDepartureUpToCurrentTime;
-import com.gridnine.testing.filters.FilterTimeOnGroundBetweenDepartures;
-import com.gridnine.testing.interfaces.Filters;
-import com.gridnine.testing.model.Flight;
 import com.gridnine.testing.model.Segment;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 public class ServiceTest {
-   // @Mock
-    FilterDepartureUpToCurrentTime filterDepartureUpToCurrentTime;
-//@Mock
+   @Mock
 Service listFilter;
     @Test
-    public void filterFalseTest(){
+    public void filterOneElementFalseTest(){
         List<Segment>flight=List.of(new Segment(LocalDateTime.now().plusSeconds(1),LocalDateTime.now().plusHours(2)),
-                new Segment(LocalDateTime.now().minusSeconds(0),LocalDateTime.now().plusHours(2)));
+                new Segment(LocalDateTime.now().minusSeconds(1),LocalDateTime.now().plusHours(2)));
         FilterDepartureUpToCurrentTime filterObject=new FilterDepartureUpToCurrentTime(flight);
-        listFilter= new Service(List.of(filterObject));
-        Mockito.when(listFilter.getFilters().get(0).filter()).thenReturn(true);
-    //    Mockito.when(listFilter.getFilters().get(1).filter()).thenReturn(false);
-       int ii=listFilter.filter().size();
-        System.out.println(ii);
-        Assertions.assertEquals(ii,1);
+        List<Segment>flight1=List.of(new Segment(LocalDateTime.now().plusSeconds(1),LocalDateTime.now().plusHours(2)),
+                new Segment(LocalDateTime.now().plusSeconds(1),LocalDateTime.now().plusHours(2)));
+        FilterDepartureUpToCurrentTime filterObject1=new FilterDepartureUpToCurrentTime(flight1);
+        listFilter= new Service(List.of(filterObject,filterObject1));
+             int ii=listFilter.filter().size();
+               Assertions.assertEquals(ii,1);
+    }
+    @Test
+    public void filterAllElementFalseTest(){
+        List<Segment>flight=List.of(new Segment(LocalDateTime.now().plusSeconds(1),LocalDateTime.now().plusHours(2)),
+                new Segment(LocalDateTime.now().plusSeconds(1),LocalDateTime.now().plusHours(2)));
+        FilterDepartureUpToCurrentTime filterObject=new FilterDepartureUpToCurrentTime(flight);
+        List<Segment>flight1=List.of(new Segment(LocalDateTime.now().plusSeconds(1),LocalDateTime.now().plusHours(2)),
+                new Segment(LocalDateTime.now().plusSeconds(1),LocalDateTime.now().plusHours(2)));
+        FilterDepartureUpToCurrentTime filterObject1=new FilterDepartureUpToCurrentTime(flight1);
+        listFilter= new Service(List.of(filterObject,filterObject1));
+        int ii=listFilter.filter().size();
+        Assertions.assertEquals(ii,2);
+    }
+    @Test
+    public void filterAllElementTrueTest(){
+        List<Segment>flight=List.of(new Segment(LocalDateTime.now().plusSeconds(1),LocalDateTime.now().plusHours(2)),
+                new Segment(LocalDateTime.now().minusSeconds(1),LocalDateTime.now().plusHours(2)));
+        FilterDepartureUpToCurrentTime filterObject=new FilterDepartureUpToCurrentTime(flight);
+        List<Segment>flight1=List.of(new Segment(LocalDateTime.now().plusSeconds(1),LocalDateTime.now().plusHours(2)),
+                new Segment(LocalDateTime.now().minusSeconds(1),LocalDateTime.now().plusHours(2)));
+        FilterDepartureUpToCurrentTime filterObject1=new FilterDepartureUpToCurrentTime(flight1);
+        listFilter= new Service(List.of(filterObject,filterObject1));
+        int ii=listFilter.filter().size();
+        Assertions.assertEquals(ii,0);
     }
 }
